2442f29a119102110755dbb7dde06b72dec3945f
Alexcppio <62553154+Alexcppio@users.noreply.github.com>
2020-09-17 17:28:54 +0500

    Add files via upload

---
+++ CurlTransfer.cpp
@@ -0,0 +1,37 @@
+﻿#include "CurlTransfer.h"
+
+static int writer(char* data, size_t size, size_t nmemb, string *sourceText)
+{
+	int result = 0;
+	if (sourceText != NULL)
+	{
+		sourceText->append(data, size * nmemb);
+		result = size * nmemb;
+	}
+	return result;
+}
+
+void initCurl(string& sourceText, string adressPath) {
+	CURL* curl;
+	CURLcode result;
+	ifstream path;
+	string temp;
+	curl = curl_easy_init();
+	adressPath += adressPath;
+
+	if (curl)
+	{
+		curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
+		curl_easy_setopt(curl, CURLOPT_URL, adressPath.c_str());
+		curl_easy_setopt(curl, CURLOPT_HEADER, 1);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writer);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &sourceText);
+		result = curl_easy_perform(curl);
+		if (result == CURLE_OK) {
+		}
+		else {
+			cout << "Îøèáêà! " << errbuf << endl;
+		}
+	}
+	curl_easy_cleanup(curl);
+}
---
+++ CurlTransfer.h
@@ -0,0 +1,30 @@
+#ifndef cURL_H
+#define cURL_H
+#define CURL_STATICLIB
+#ifdef _DEBUG
+#pragma comment (lib, "curl/libcurl_a_debug.lib")
+#else
+#pragma comment (lib, "../curl/libcurl_a.lib")
+#endif
+#pragma comment (lib, "Normaliz.lib")
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Wldap32.lib")
+#pragma comment (lib, "advapi32.lib")
+#pragma comment (lib, "Crypt32.lib")
+
+#include <iostream>
+#include <string>
+#include "../curl/curl.h"
+#include <fstream>
+using std::string;
+using std::cout;
+using std::endl;
+using std::ifstream;
+#include <iostream>
+#include <string>
+
+static char errbuf[CURLOPT_ERRORBUFFER];
+static int writer(char* data, size_t size, size_t nmemb, string* sourceText);
+void initCurl(string &sourceText, string adressPath);
+
+#endif cURL_H
